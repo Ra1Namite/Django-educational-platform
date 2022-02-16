58f928810961483af41aff11c67ebea45b813335
Sanjaya Rai <rainamite@gmail.com>
2022-02-16 18:54:00 +0545

    dockerize the project

--- app/config/settings.py
+++ app/config/settings.py
@@ -20,12 +20,14 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/3.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '!2*sb0n9$rh(j5_ku$!c8+2mhp3)t!1n306)ltn5rq^jjodqb+'
+SECRET_KEY = os.environ.get("SECRET_KEY")
+
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = int(os.environ.get("DEBUG", default=0))
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = os.environ.get("DJANGO_ALLOWED_HOSTS").split(" ")
+
 
 
 # Application definition
